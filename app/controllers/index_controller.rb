e3367ade459fa150684b8e472269b20164673706
Han Kim <hankim813@gmail.com>
2014-12-02 12:33:56 -0800

    wip -- out to lunch
    - finish the reponse from ajax call

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,12 @@
 get '/' do
+	redirect "/families/#{current_user.family.id}/show" if current_user
+	require_relative "../../config"
+	@app_id = APP_ID
   # Landing Page
   erb :index
+end
+
+get '/api/fb/user' do
+	p params
 end
 
